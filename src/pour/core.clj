27a130de9f2bda4320ab2f33e4d9e8d1eeb91132
Dan Peddle <dan@flarework.com>
2020-10-21 21:03:06 +0100

    prefer core async to futures

--- src/pour/core.clj
+++ src/pour/core.clj
@@ -1,9 +1,15 @@
 (ns pour.core
-  (:require [edn-query-language.core :as eql])
+  (:require [edn-query-language.core :as eql]
+            [clojure.core.async :as ca])
   (:import (clojure.lang Seqable)))
 
+(def ^{:private true
+       :dynamic true
+       :doc     "Results channel for resolvers per invocation of parse"}
+  *chan* nil)
+
 (defn seqy? [s]
-  (and (not (:db/id s)) ; ie, a datomic Entity or similar
+  (and (not (:db/id s))
        (not (map? s))
        (instance? Seqable s)))
 
@@ -12,65 +18,76 @@
 
 (defn knit [{:keys [resolvers] :as env}
             {:keys [dispatch-key value] :as node}]
-  (future (let [resolver (or (get resolvers dispatch-key)
-                             (fn default-resolver [_ _]
-                               (get value dispatch-key)))]
-            (resolver env (merge node {:value value})))))
+  (let [resolver (or (get resolvers dispatch-key)
+                     (fn default-resolver [_ _]
+                       (get value dispatch-key)))]
+    (resolver env (merge node {:value value}))))
 
 (defn- matches-union [key value]
   (boolean (key value)))
 
-(defn- process-map [m]
-  (if (map? m)
-    (->> m
-         (keep (fn [[k v]]
-                 (let [vv (if (future? v)
-                            (deref v)
-                            v)]
-                   (when (not (nil? vv))
-                     [k vv]))))
-         (into {}))
-    m))
+(declare parse)
 
-(defn parse [env {:keys [value children] :as node}]
+(defn process-child [{:keys [env value child node-params]}]
+  (let [on-error (or (:on-error env) (fn [t]))
+        resolved (try
+                   (knit env (merge {:value value} child))
+                   (catch Throwable t
+                     (on-error t)))
+        {:keys [type key params]} child
+        result (case type
+                 :prop resolved
+                 :union (let [union-dispatch (or (when-let [custom-dispatch (:union-dispatch node-params)]
+                                                   (and (symbol? custom-dispatch)
+                                                        (resolve custom-dispatch)))
+                                                 matches-union)]
+                          (reduce (fn [_ {:keys [union-key children params] :as uc}]
+                                    (when (union-dispatch union-key value)
+                                      (reduced (parse env {:value    value
+                                                           :children children}))))
+                                  {}
+                                  (:children child)))
+                 :join (if (seqy? resolved)
+                         (->> resolved
+                              (keep (fn [v]
+                                      (parse env (merge {:value v} child))))
+                              (into []))
+                         (parse env (merge {:value resolved} child)))
+                 nil)
+        result (if (nil? result)
+                 (:default params)
+                 result)
+        kname (or (:as params) key)]
+    (if kname
+      [kname result]
+      result)))
+
+(defn parse [env {:keys [value children]
+                  node-params :params}]
   (when value
-    (let [node-params (:params node)]
-      (->> children
-           (map (fn [child]
-                  {:pending (knit env (merge {:value value} child))
-                   :child child}))
-           (reduce (fn [acc {:keys [pending child]}]
-                     (let [resolved (deref pending)
-                           {:keys [type key params]} child
-                           v (condp = type
-                               :prop resolved
-                               :union (let [union-dispatch (or (when-let [custom-dispatch (:union-dispatch node-params)]
-                                                                 (and (symbol? custom-dispatch)
-                                                                      (resolve custom-dispatch)))
-                                                               matches-union)]
-                                        (reduce (fn [_ {:keys [union-key children params] :as uc}]
-                                                  (when (union-dispatch union-key value)
-                                                    (reduced (parse env {:value    value
-                                                                         :children children}))))
-                                                {}
-                                                (:children child)))
-                               :join (if (seqy? resolved)
-                                       (->> resolved
-                                            (keep (fn [v]
-                                                    (parse env (merge {:value v} child))))
-                                            (into []))
-                                       (parse env (merge {:value resolved} child)))
-                               nil)
-                           result (or v (:default params))
-                           kname (or (:as params) key)]
-                       (if (not (nil? result))
-                         (if kname
-                           (assoc acc kname result)
-                           result)
-                         acc)))
-                   {})
-           process-map))))
-
+    (binding [*chan* (ca/chan)]
+      (let [expected-results (count children)
+            wrapping? (nil? (get-in children [0 :key]))]
+        (doseq [child children]
+          (ca/go (ca/>! *chan* (process-child {:env env
+                                               :value value
+                                               :child child
+                                               :node-params node-params}))))
+        (ca/<!! (ca/go-loop [result {}
+                             pending-results expected-results]
+                  (let [out (ca/<! *chan*)
+                        next-pending-count (dec pending-results)
+                        finished? (zero? next-pending-count)]
+                    (if wrapping?
+                      out
+                      (let [[k v] out
+                            next-result (if (nil? v)
+                                          result
+                                          (assoc result k v))]
+                        (if finished?
+                          next-result
+                          (recur next-result
+                                 next-pending-count)))))))))))
 
 (defn pour
   ([query value]
