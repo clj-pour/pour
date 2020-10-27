1fa5b256e1c58e18f6d61462e2e33db98e272aa7
Dan Peddle <dan@flarework.com>
2020-10-27 17:12:25 +0000

    only one blocking call

--- src/pour/core.clj
+++ src/pour/core.clj
@@ -1,12 +1,8 @@
 (ns pour.core
   (:require [edn-query-language.core :as eql]
-            [clojure.core.async :as ca])
+            [clojure.core.async :as ca]
+            [clojure.tools.logging :as log])
   (:import (clojure.lang Seqable)))
-
-(def ^{:private true
-       :dynamic true
-       :doc     "Results channel for resolvers per invocation of parse"}
-  *chan* nil)
 
 (defn seqy? [s]
   (and (not (:db/id s))
@@ -29,7 +25,8 @@
 (declare parse)
 
 (defn process-child [{:keys [env value child node-params]}]
-  (let [on-error (or (:on-error env) (fn [t]))
+  (log/info ::pc value)
+  (let [on-error (or (:on-error env) (fn [t] (log/error t)))
         resolved (try
                    (knit env (merge {:value value} child))
                    (catch Throwable t
@@ -43,16 +40,16 @@
                                                  matches-union)]
                           (reduce (fn [_ {:keys [union-key children params] :as uc}]
                                     (when (union-dispatch union-key value)
-                                      (reduced (parse env {:value    value
-                                                           :children children}))))
+                                      (reduced (ca/<! (parse env {:value    value
+                                                                  :children children})))))
                                   {}
                                   (:children child)))
                  :join (if (seqy? resolved)
                          (->> resolved
                               (keep (fn [v]
-                                      (parse env (merge {:value v} child))))
+                                      (ca/<! (parse env (merge {:value v} child)))))
                               (into []))
-                         (parse env (merge {:value resolved} child)))
+                         (ca/<! (parse env (merge {:value resolved} child))))
                  nil)
         result (if (nil? result)
                  (:default params)
@@ -60,42 +57,57 @@
         kname (or (:as params) key)]
     (if kname
       [kname result]
-      result)))
+      ;; change to ::nil
+      (if (nil? result)
+        ::nil
+        result))))
 
-(defn parse [env {:keys [value children]
+(defn parse [env {:keys       [value children key]
                   node-params :params}]
+  (prn ::k key)
   (when value
-    (binding [*chan* (ca/chan)]
-      (let [expected-results (count children)
+    (binding []
+      (let [chan (ca/chan)
+            expected-results (count children)
             wrapping? (nil? (get-in children [0 :key]))]
         (doseq [child children]
-          (ca/go (ca/>! *chan* (process-child {:env env
-                                               :value value
-                                               :child child
-                                               :node-params node-params}))))
-        (ca/<!! (ca/go-loop [result {}
-                             pending-results expected-results]
-                  (let [out (ca/<! *chan*)
-                        next-pending-count (dec pending-results)
-                        finished? (zero? next-pending-count)]
-                    (if wrapping?
-                      out
-                      (let [[k v] out
-                            next-result (if (nil? v)
-                                          result
-                                          (assoc result k v))]
-                        (if finished?
-                          next-result
-                          (recur next-result
-                                 next-pending-count)))))))))))
+          (ca/go (ca/>! chan (process-child {:env         env
+                                             :value       value
+                                             :child       child
+                                             :node-params node-params}))))
+
+        (ca/go-loop [result {}
+                     pending-results expected-results]
+          (let [next-pending-count (dec pending-results)
+                finished? (zero? next-pending-count)
+
+                next-result (try
+                              (let [out (ca/<! chan)
+                                    _ (log/info ::out out)]
+                                ;; change to ::nil sentinel
+                                (if wrapping?
+                                  (if (= ::nil out) nil out)
+                                  (let [[k v] out]
+                                    (if (nil? v)
+                                      result
+                                      (assoc result k v)))))
+                              (catch Throwable t
+                                (log/error ::uhoh t)
+                                result))]
+            (if finished?
+              next-result
+              (recur next-result
+                     next-pending-count))))))))
 
 (defn pour
   ([query value]
    (pour {} query value))
   ([env query value]
+   (prn ::hi)
    (let [env (update env :resolvers merge {:pipe pipe})
          ast (eql/query->ast query)]
      (->> {:value value}
           (merge ast)
-          (parse env)))))
+          (parse env)
+          (ca/<!!)))))
 
