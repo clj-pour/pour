1dc819ccfe5396137e6c29a55fbb5cde19f3745c
Dan Peddle <dan@flarework.com>
2021-03-23 22:39:14 +0000

    simplify query, retire topo sort, introduce ::pc/ident on meta

--- src/pour/compose.clj
+++ src/pour/compose.clj
@@ -1,45 +1,23 @@
 (ns pour.compose
   (:require [clojure.walk :as walk]
-            [loom.graph :as g]
-            [loom.alg :as alg]
             [pour.core :as pour]))
 
 (defn query
-  ([component]
-   (:query (meta component)))
-  ([kw component]
-   (into [(list ::renderer {:default kw})
-          (list ::render-fn {:default component})]
-         (query component))))
+  [component]
+  (into [(list ::renderer {:default component})]
+        (:query (meta component))))
 
-(defn queries [renderers]
-  (->> renderers
-       (map (fn [[k v]] [k (query k v)]))
-       (into {})))
-
-(defn find-vars [query]
-  (filter symbol? (tree-seq coll? seq query)))
-
-(defn dep-map [config]
-  (zipmap (keys config)
-          (map #(set (find-vars (query %)))
-               (vals config))))
-
-(defn dep-order [config]
-  (-> config dep-map g/digraph alg/topsort reverse))
-
-(defn inject-query [queries query]
+(defn inject-query [renderers unprocessed-query]
   (walk/prewalk (fn [query-part]
                   (if (symbol? query-part)
-                    (get queries (keyword query-part) query-part)
+                    (let [component (get renderers (keyword query-part))
+                          {raw-query :query} (meta component)]
+                      (if raw-query
+                        (query component)
+                        (throw (ex-info (str "Missing renderer for " query-part)
+                                        {:query-part query-part}))))
                     query-part))
-                query))
-
-(defn resolve-all-deps [queries]
-  (reduce (fn [queries' k]
-            (update queries' k #(inject-query queries %)))
-          queries
-          (dep-order queries)))
+                unprocessed-query))
 
 (defn validate-query [q]
   (if-not (vector? q)
@@ -89,15 +67,14 @@
         resolved-query# (walk/prewalk (fn [query-part]
                                         (if (symbol? query-part)
                                           (if-let [rvar# (resolve &env query-part)]
-                                            (let [kw# (keyword (symbol rvar#))
-                                                  ;; pick up unresolved renderers from children
+                                            (let [;; pick up unresolved renderers from children
                                                   qp-unresolveds# (::unresolved (meta (deref rvar#)))
                                                   mq# (:query (meta (deref rvar#)))]
                                               ;; merge into accumulated unresolveds at this level
                                               (when (seq qp-unresolveds#)
                                                 (swap! !unresolved-symbols# into qp-unresolveds#))
                                               (if mq#
-                                                (query kw# (deref rvar#))
+                                                (query (deref rvar#))
                                                 rvar#))
                                             (do
                                               ;; the passed symbol doesn't correspond to anything we can resolve
@@ -107,6 +84,10 @@
                                           query-part))
                                       query-literal)
         query-errors# (validate-query resolved-query#)
+        ident# (or (some-> (resolve &env cup-name)
+                           (symbol)
+                           (keyword))
+                   ::unknown)
         unresolveds# @!unresolved-symbols#]
     (when (seq query-errors#)
       (throw (ex-info "Query Error" {:type   ::query-error
@@ -115,36 +96,22 @@
        (with-meta ~body
                   (merge (meta ~body)
                          {::unresolved '~unresolveds#
+                          ::ident      ~ident#
                           :query       '~resolved-query#})))))
 
-(defn render2
+(defn render
   ([renderer value]
-   (render2 {} renderer value))
+   (render {} renderer value))
   ([env renderer value]
    (let [m (meta renderer)
-         query (:query m)
          unresolveds (::unresolved m)
-         renderers (::renderers env)]
-         ;; zip up the unresolveds with supplied renderers and error if something missing
+         renderers (::renderers env)
+         query (cond->> (:query m)
+                        (seq unresolveds) (inject-query renderers))]
      (with-meta (->> (pour/pour (or env {})
                                 query
                                 value)
                      (renderer))
                 {:renderer renderer
                  :query    query}))))
-
-(defn render
-  "for a given map of `renderers`, invoke the renderer `root-renderer` with root value `root-value`
-  using the supplied `fetch` function"
-  [fetch renderers root-renderer root-value]
-  (let [queries (-> renderers queries resolve-all-deps)
-        renderer (if (fn? root-renderer)
-                   root-renderer
-                   (root-renderer renderers))
-        query (root-renderer queries)]
-    (with-meta (or (->> (fetch query root-value)
-                        (renderer))
-                   {})
-               {:renderer renderer
-                :query    query})))
 
